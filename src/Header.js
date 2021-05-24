6ef799c4c833075d74ff40d0a074fd7017082b9e
Kian Sam <itskiansam@gmail.com>
2021-05-24 12:27:55 +0300

    complete

--- src/Header.js
+++ src/Header.js
@@ -3,31 +3,50 @@ import styled from "styled-components";
 const Header = (props) => {
   return (
     <Container>
-      <Wrap>
-        {" "}
+      <Logo>
         <img src="/images/Tree.png" alt="" />
-        <h1>Hi</h1>
-      </Wrap>
+      </Logo>
+      <Wrap>{/* Icons, Tags, ... */}</Wrap>
     </Container>
   );
 };
 
 const Container = styled.div`
+  position: fixed;
   background-color: #000;
+  top: 0;
+  left: 0;
+  right: 0;
   width: 100%;
   height: 90px;
-  margin: 0;
   display: flex;
-  justify-content: center;
-  text-align: center;
+  justify-content: space-between;
+  padding: 0 30px;
   align-items: center;
+  z-index: 1000;
+`;
+
+const Logo = styled.a`
+  /* padding: 0; */
+  width: 80px;
+  /* font-size: 0; */
+  /* display: inline-block; */
+
+  img {
+    /* display: flex; */
+    width: 100%;
+    border-radius: 50px;
+    align-items: center;
+  }
 `;
 
 const Wrap = styled.div`
-  color: #fff;
-  width: 20%;
-  height: 60px;
-  background-color: #fff;
+  align-items: center;
+  display: flex;
+  flex-flow: row nowrap;
+  position: relative;
+  margin-right: auto;
+  margin-left: auto;
 `;
 
 export default Header;
