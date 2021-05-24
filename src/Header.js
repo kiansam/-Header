49a028a2f34cc78f4fc4eccd79e78bffd42f030a
Kian Sam <itskiansam@gmail.com>
2021-05-24 12:03:17 +0300

    Second Commit

--- src/Header.js
+++ src/Header.js
@@ -4,6 +4,8 @@ const Header = (props) => {
   return (
     <Container>
       <Wrap>
+        {" "}
+        <img src="/images/Tree.png" alt="" />
         <h1>Hi</h1>
       </Wrap>
     </Container>
@@ -11,13 +13,21 @@ const Header = (props) => {
 };
 
 const Container = styled.div`
+  background-color: #000;
+  width: 100%;
+  height: 90px;
+  margin: 0;
   display: flex;
   justify-content: center;
   text-align: center;
+  align-items: center;
 `;
 
 const Wrap = styled.div`
-  margin: 0;
+  color: #fff;
+  width: 20%;
+  height: 60px;
+  background-color: #fff;
 `;
 
 export default Header;
