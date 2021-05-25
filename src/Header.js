11abfd49e9ef1492194e4138cf9a6521c6f784cf
Kian Sam <itskiansam@gmail.com>
2021-05-25 10:21:06 +0300

    NavMenu complete

--- src/Header.js
+++ src/Header.js
@@ -6,6 +6,17 @@ const Header = (props) => {
       <Logo>
         <img src="/images/Tree.png" alt="" />
       </Logo>
+      <NavMenu>
+        <a href="/home">
+          <span>Home</span>
+        </a>
+        <a href="/about">
+          <span>About us</span>
+        </a>
+        <a href="/contact">
+          <span>Contact us</span>
+        </a>
+      </NavMenu>
       <Wrap>{/* Icons, Tags, ... */}</Wrap>
       <Login>Login</Login>
     </Container>
@@ -51,10 +62,69 @@ const Wrap = styled.div`
   margin-left: auto;
 `;
 
+const NavMenu = styled.div`
+  align-items: center;
+  display: flex;
+  flex-flow: row nowrap;
+  height: 100%;
+  justify-content: flex-end;
+  margin: 0;
+  padding: 0;
+  position: relative;
+  margin-right: auto;
+  margin-left: 30px;
+
+  a {
+    text-decoration: none;
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+
+    span {
+      color: rgb(249, 249, 249);
+      font-size: 18px;
+      letter-spacing: 1px;
+      line-height: 1.08;
+      padding: 1px 0;
+      white-space: nowrap;
+      position: relative;
+
+      &:before {
+        background-color: rgb(249, 249, 249);
+        border-radius: 0 0 4px 4px;
+        bottom: -6px;
+        content: "";
+        height: 2px;
+        left: 0;
+        opacity: 0;
+        position: absolute;
+        right: 0;
+        transform-origin: left center;
+        transform: scaleX(0);
+        transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+        visibility: hidden;
+        width: auto;
+      }
+    }
+
+    &:hover {
+      span:before {
+        transform: scaleX(1);
+        visibility: visible;
+        opacity: 1 !important;
+      }
+    }
+  }
+
+  @media (max-width: 548px) {
+    display: none;
+  }
+`;
+
 const Login = styled.a`
   color: #ffffff;
   background-color: rgba(0, 0, 0, 0.6);
-  padding: 8px 16px;
+  padding: 10px 16px;
   margin-right: 45px;
   text-transform: uppercase;
   letter-spacing: 1.5px;
