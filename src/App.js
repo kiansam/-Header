ca1da6897dad48a16711577bf66e09044cf3c9b2
Kian Sam <itskiansam@gmail.com>
2021-05-26 09:41:18 +0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/"></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
