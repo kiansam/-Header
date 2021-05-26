cb9b1830581b0f6dfb52d52c452a44499cb9eb6e
Kian Sam <itskiansam@gmail.com>
2021-05-26 11:13:10 +0300

    About-us1

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,7 +1,20 @@
 import styled from "styled-components";
 
 const About = (props) => {
-  return <div></div>;
+  return (
+    <Container>
+      <h1>Kian</h1>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  align-items: center;
+  display: flex;
+  margin-top: 100px;
+  margin-right: auto;
+  margin-left: auto;
+  justify-content: center;
+`;
 
 export default About;
